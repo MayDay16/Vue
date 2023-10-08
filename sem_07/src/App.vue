3b6e2302563684f6fdc2c9e8647fc325b6c34923
MayDay16 <110927948+MayDay16@users.noreply.github.com>
2023-10-08 16:42:56 +0500

    Add_sem_07

---
+++ sem_07/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <CounterComp/>
+    <CounterComp2/>
+    <CartItems/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import CounterComp from './components/CounterComp.vue';
+import CounterComp2 from './components/CounterComp2.vue';
+import CartItems from './components/CartItems.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    CounterComp,
+    CounterComp2,
+    CartItems
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
